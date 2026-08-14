0517c720a31129f7999653f4fc69ac111f7464ba
ctwadeguzmanqueenbsis2b-bit <ct.wa.deguzman.queen.bsis2b@gmail.com>
2026-08-14 09:47:00 +0800

    Add files via upload

---
+++ araalei/act2_deguzman_queen.php
@@ -0,0 +1,58 @@
+<?php
+
+// Nickname: ara
+
+/*
+Name: Queen Ara De Guzman
+Age: 18
+Section: BSIS-2B
+Address: Gatbuca,calimpit,Bulacan
+Hobbies: Watching movies, Listening to music
+Dream Job: Web Developer
+Course: Bachelor of Science in Information Systems
+School: Polytechnic College of the City of Malolos
+*/
+
+$me = array(
+    "name" => "Queen Ara De Guzman",
+    "age" => 18,
+    "section" => "BSIS-2B",
+    "address" => "Gatbuca,calimpit,Bulacan",
+    "hobbies" => "Watching movie, Listening to music",
+    "dream_job" => "Web Developer",
+    "course" => "Bachelor of Science in Information Systems",
+    "school" => "Bulacan Polytechnic College"
+);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About Me</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div class="box">
+        <h1>About Me</h1>
+
+        <p><strong>Name:</strong> <?php echo $me["name"]; ?></p>
+        <p><strong>Age:</strong> <?php echo $me["age"]; ?></p>
+        <p><strong>Section:</strong> <?php echo $me["section"]; ?></p>
+        <p><strong>Address:</strong> <?php echo $me["address"]; ?></p>
+        <p><strong>Hobbies:</strong> <?php echo $me["hobbies"]; ?></p>
+        <p><strong>Dream Job:</strong> <?php echo $me["dream_job"]; ?></p>
+        <p><strong>Course:</strong> <?php echo $me["course"]; ?></p>
+        <p><strong>School:</strong> <?php echo $me["school"]; ?></p>
+
+        <p class="highlight">
+            <?php 
+            print "I am " . $me["name"] . ", a " . $me["course"] . 
+            " student of " . $me["school"] . ".";
+            ?>
+        </p>
+    </div>
+</body>
+</html>
